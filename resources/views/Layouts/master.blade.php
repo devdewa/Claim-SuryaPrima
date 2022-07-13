306947758b8ab2f2d84532eaa869bcdd15232902
unknown <uchiasasuke123.sp@gmail.com>
2022-07-13 11:09:08 +0700

    Update

--- resources/views/Layouts/master.blade.php
+++ resources/views/Layouts/master.blade.php
@@ -7,15 +7,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="Adityawarman Dewa Putra">
 
     <title>@yield('title')</title>
 
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('asset/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('asset/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('asset/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('asset/favicon/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('asset/favicon/safari-pinned-tab.svg') }}" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#603cba">
+    <meta name="theme-color" content="#ffffff">
+
     <!-- Custom fonts for this template-->
     <link href="{{ asset('/asset/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="{{ asset('/asset/css/sb-admin-2.min.css') }}" rel="stylesheet">
